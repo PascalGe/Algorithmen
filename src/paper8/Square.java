7c4860560d503f3ee6ade4e246032ef125c7aee9
Pascal <i13004@hb.dhbw-stuttgart.de>
2019-12-10 23:03:04 +0100

    Update exercise 8.3
    - Formatted Square.

--- src/paper8/Square.java
+++ src/paper8/Square.java
@@ -1,22 +1,21 @@
 package paper8;
 
 public class Square {
-	
+
 	public static final int EMPTY = 0; // square is empty
 	public static final int WHITE = 1; // square is white
 	public static final int BLACK = 2; // square is black
 
-	
-	private int content; 	// One of EMPTY, WHITE or BLACK
-	
-	// every square has hash values depending on its state
-	private int hashEmpty; // hash if square is 	empty
-	private int hashWhite; // 						white
-	private int hashBlack; //						black
+	private int content; // One of EMPTY, WHITE or BLACK
 
+	// every square has hash values depending on its state
+	private int hashEmpty; // hash if square is empty
+	private int hashWhite; // white
+	private int hashBlack; // black
 
 	/**
 	 * Creates a new square
+	 * 
 	 * @param content   content of this square
 	 * @param hashEmpty hash value for empty
 	 * @param hashWhite hash value for white
@@ -27,11 +26,11 @@ public class Square {
 		this.hashEmpty = hashEmpty;
 		this.hashWhite = hashWhite;
 		this.hashBlack = hashBlack;
-	} 
+	}
 
-	
 	/**
 	 * Returns the current hash value of this square
+	 * 
 	 * @return hash value
 	 */
 	public int getHash() {
@@ -39,18 +38,14 @@ public class Square {
 		if (this.content == EMPTY) {
 			return hashEmpty;
 
-		}
-		else if (this.content == WHITE) {
+		} else if (this.content == WHITE) {
 			return hashWhite;
-		}
-		else {
+		} else {
 			return hashBlack;
-		}		
+		}
 
 	}
 
-
-	
 	public int getHashEmpty() {
 		return hashEmpty;
 	}
@@ -58,8 +53,6 @@ public class Square {
 	public void setHashEmpty(int hashEmpty) {
 		this.hashEmpty = hashEmpty;
 	}
-
-
 
 	public int getHashWhite() {
 		return hashWhite;
@@ -69,8 +62,6 @@ public class Square {
 		this.hashWhite = hashWhite;
 	}
 
-
-
 	public int getHashBlack() {
 		return hashBlack;
 	}
@@ -79,24 +70,25 @@ public class Square {
 		this.hashBlack = hashBlack;
 	}
 
-
-
 	public int getContent() {
 		return content;
 	}
-	
+
 	public void setContent(int content) {
 		this.content = content;
 	}
 
-	
 	public String draw() {
 
-		switch(this.content){
-		case EMPTY: return " ";
-		case WHITE: return "o";
-		case BLACK: return "x";
-		default: return "something nasty happend...";
+		switch (this.content) {
+		case EMPTY:
+			return " ";
+		case WHITE:
+			return "o";
+		case BLACK:
+			return "x";
+		default:
+			return "something nasty happend...";
 		}
 	}
 }
