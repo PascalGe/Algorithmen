f7ccb84e1c8e6ea586dd647fdfdce07796e6eeb8
Pascal <i13004@hb.dhbw-stuttgart.de>
2019-12-10 22:32:24 +0100

    Update exercise 8.3 a), b)
    - Implemented computeHash.
    - Implemented change.

--- src/paper8/Board.java
+++ src/paper8/Board.java
@@ -6,10 +6,8 @@ import java.util.Random;
 public class Board {
 
 	public ArrayList<Square> board; // Board of 8 x 8 =64 Squares
-	public int               hash;
+	public int hash;
 
-	
-	
 	/**
 	 * Initialize a new board where the individual hashes are randomly generated
 	 */
@@ -17,84 +15,90 @@ public class Board {
 
 		this.board = new ArrayList<Square>();
 
-		for (int i = 0; i < 64; i++){
+		for (int i = 0; i < 64; i++) {
 			int hashEmpty = newHash();
 			int hashWhite = newHash();
 			int hashBlack = newHash();
 			Square s = new Square(Square.EMPTY, hashEmpty, hashWhite, hashBlack);
 			board.add(s);
-
 		}
 		this.hash = computeHash();
 	}
 
-
 	/**
-	 * Randomly generates hash values.
-	 * (No collision check, the hash values could be the same,
-	 * but that is very unlikely.)
+	 * Randomly generates hash values. (No collision check, the hash values could be
+	 * the same, but that is very unlikely.)
+	 * 
 	 * @return
 	 */
 	private int newHash() {
-		Random rand   = new Random();
+		Random rand = new Random();
 		int randomNum = rand.nextInt();
 		return randomNum;
 	}
 
-
 	/**
-	 * Computes hash of the board in the initialization process. Not to be used when changing the game state.
-	 * DANGER: USING THIS FUNCTION WHILE CHANGING THE GAME STATE, MIGHT LEAD TO SEVERE INJURY AND LOSS OF ÜBUNGSPOINTS.
+	 * Computes hash of the board in the initialization process. Not to be used when
+	 * changing the game state. DANGER: USING THIS FUNCTION WHILE CHANGING THE GAME
+	 * STATE, MIGHT LEAD TO SEVERE INJURY AND LOSS OF ÜBUNGSPOINTS.
+	 * 
 	 * @return
 	 */
 	private int computeHash() {
-		
-		//TODO: IMPLEMENT ME
-		
-		return 0;
+
+		// TODO: IMPLEMENT ME
+		int hash = 0;
+		for (int i = 0; i < board.size(); i++) {
+			System.out.println(i);
+			if (board.get(i).getContent() != Square.EMPTY) {
+				hash = hash ^ board.get(i).getHash();
+			}
+		}
+		return hash;
 	}
 
-
-
-
 	/**
-	 * The value of one board position is changed.
-	 * Student exercise 
-	 * @param position
+	 * The value of one board position is changed. Student exercise
+	 * 
+	 * @param position 0 to 63
 	 * @param newState
 	 * @return
 	 */
 	public int change(int position, int newState) {
-		
-		//TODO: IMPLEMENT ME
-		
-		return 0;
+
+		// TODO: IMPLEMENT ME
+		// get current square
+		Square square = board.get(position);
+		// hash out old content
+		hash ^= square.getContent();
+		// set new content
+		square.setContent(newState);
+		// hash in new content
+		hash ^= square.getContent();
+		return hash;
 	}
-	
-	
+
 	/**
 	 * print the current board content
+	 * 
 	 * @return
 	 */
 	public String draw() {
 		String coolMinus = "\u2014";
 		String sep = "";
-		
-		for(int i = 0; i < 17; i++){
+
+		for (int i = 0; i < 17; i++) {
 			sep += coolMinus;
 		}
 		sep += "\n";
 
 		String s = sep;
-		for (int i = 0; i <64; i++){
+		for (int i = 0; i < 64; i++) {
 			s += "|" + this.board.get(i).draw();
-			if(i % 8 == 7){
-				s+= "|\n" + sep;
+			if (i % 8 == 7) {
+				s += "|\n" + sep;
 			}
-
 		}
 		return s;
 	}
-
 }
-
